7bb0656c9bf92a6b16ebc54473d85e967854b5e5
Sten <stentechnology2005@gmail.com>
2024-06-12 15:52:45 +0200

    Initial commit

---
+++ src/client/java/com/stencode/breakable/BreakableClient.java
@@ -0,0 +1,50 @@
+package com.stencode.breakable;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.jetbrains.annotations.NotNull;
+
+public class BreakableClient implements ClientModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Loading Breakable...");
+		ModConfig.init();
+
+		UseItemCallback.EVENT.register((player, world, hand) -> {
+			ItemStack itemStack = player.getStackInHand(hand);
+			if (ModConfig.INSTANCE.enabled && itemStack.isDamageable() && itemStack.getMaxDamage() - itemStack.getDamage() <= 1) {
+				if (ModConfig.INSTANCE.showNotification)
+					player.sendMessage(Text.of("Item is too damaged to use!"), true);
+				return TypedActionResult.fail(itemStack);
+			}
+
+			return TypedActionResult.pass(itemStack);
+		});
+
+		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> blockCallback(player, hand));
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> blockCallback(player, hand));
+	}
+
+	@NotNull
+	private ActionResult blockCallback(PlayerEntity player, Hand hand) {
+		ItemStack itemStack = player.getStackInHand(hand);
+		if (ModConfig.INSTANCE.enabled && itemStack.isDamageable() && itemStack.getMaxDamage() - itemStack.getDamage() <= 1) {
+			if (ModConfig.INSTANCE.showNotification)
+				player.sendMessage(Text.of("Item is too damaged to use!"), true);
+			return ActionResult.FAIL;
+		}
+		return ActionResult.PASS;
+	}
+}
---
+++ src/client/java/com/stencode/breakable/ModConfig.java
@@ -0,0 +1,27 @@
+package com.stencode.breakable;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+
+@Config(name = "breakable")
+public class ModConfig implements ConfigData {
+
+    @ConfigEntry.Gui.Excluded
+    public static ModConfig INSTANCE;
+
+    public static void init()
+    {
+        AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
+        INSTANCE = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    }
+
+    @ConfigEntry.Gui.Tooltip()
+    public boolean enabled = true;
+
+    @ConfigEntry.Gui.Tooltip()
+    public boolean showNotification = true;
+
+}
